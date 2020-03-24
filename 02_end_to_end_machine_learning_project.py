c81e42309d6c64f89323bb7a9d5c632e83e290e9
JulioSanchezD <julioe.sanchezd@gmail.com>
2020-03-24 08:38:44 -0600

    Added dataset split

--- 02_end_to_end_machine_learning_project.py
+++ 02_end_to_end_machine_learning_project.py
@@ -33,5 +33,26 @@ housing = load_housing_data()
 housing["ocean_proximity"].value_counts()
 
 # Plot dataframe (histogram)
-import matplotlib as plt
+import numpy as np
 housing.hist(bins=50, figsize=(20, 15))
+
+# Clasify median income for further data split
+housing["income_cat"] = pd.cut(housing["median_income"],
+                               bins=[0, 1.5, 3, 4.5, 6, np.inf],
+                               labels=[1, 2, 3, 4, 5])
+housing["income_cat"].hist()
+
+# Split data into test and train sets using stratified sampling
+from sklearn.model_selection import StratifiedShuffleSplit
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+# Proof
+strat_test_set["income_cat"].value_counts()*100 / len(strat_test_set)
+
+# Remove invome_cat columns
+for set_ in (strat_test_set, strat_train_set):
+    set_.drop("income_cat", axis=1, inplace=True)
